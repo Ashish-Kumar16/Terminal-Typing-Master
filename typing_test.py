8f3b73b64a7f6810015c8ec1aada60e0fe3e46b3
Ashish-0016 <ashish.as6021@gmail.com>
2024-01-18 22:52:56 +0530

    Refactor load_words_from_json function

--- typing_test.py
+++ typing_test.py
@@ -89,7 +89,8 @@ def load_words_from_json():
     try:
         with open(WORDS_FILE_PATH, 'r') as file:
             words_data = json.load(file)
-            return words_data.get("words", [])
+            words = words_data.get("words", [])
+            return words
     except (FileNotFoundError, json.JSONDecodeError):
         print("Error: Unable to load words. Please check the format of 'words.json'.")
         sys.exit(1)
